09475561b6352b7fbaae764370a41f6ebf3f7ef6
Rafael Ferreira <rafaelcardoso990@gmail.com>
2022-02-10 10:09:10 -0300

    implementação js, nav bar lateral

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirNav(){
+    const navBar = document.querySelector('.barra-lateral')
+    navBar.classList.toggle('hidden')
+}
+
+function fecharNav(){
+    const navBar = document.querySelector('.barra-lateral')
+    navBar.classList.toggle('hidden')
+}
